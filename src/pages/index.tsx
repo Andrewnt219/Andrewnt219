c7a9c8c6241cd175defed79b03767d4a3fdd0bf0
Tuan Phong Nguyen <phongnguyentuan20@gmail.com>
2020-08-05 22:26:51 -0400

    set up tailwind

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+import tw from "tailwind.macro";
+// import tw from "twin.macro";
+
+export default function Home() {
+  return <Button>Hi</Button>;
+}
+
+const Button = styled.button`
+  ${tw`text-xl bg-red-500`}
+`;

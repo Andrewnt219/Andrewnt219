bcb2b76bec66059f4d1ac489a7be25cc67cd03d4
Tuan Phong Nguyen <phongnguyentuan20@gmail.com>
2020-08-12 13:24:59 -0400

    added MediaIcon to Footer

---
+++ src/components/footer/MediaIcon.tsx
@@ -0,0 +1,77 @@
+import React, { ReactElement } from "react";
+import { IconType } from "react-icons";
+import styled from "styled-components";
+import tw from "twin.macro";
+
+export type MediaIconProps = {
+  Icon: IconType;
+  iconColor: IconWrapperProps["iconColor"];
+  href: string;
+  label: string;
+};
+
+function MediaIcon({
+  Icon,
+  iconColor,
+  label,
+  ...anchorAttributes
+}: MediaIconProps): ReactElement {
+  return (
+    <Container>
+      <IconWrapper
+        // html attributes
+        {...anchorAttributes}
+        target="_blank"
+        rel="noopener"
+        aria-label={label}
+        title={label}
+        // styling
+        iconColor={iconColor}
+      >
+        <Icon />
+      </IconWrapper>
+    </Container>
+  );
+}
+
+type ContainerProps = {};
+const Container = styled.div<ContainerProps>``;
+
+type IconWrapperProps = {
+  iconColor: string;
+};
+const IconWrapper = styled.a<IconWrapperProps>`
+  ${tw`inline-flex justify-center items-center w-12 h-12 bg-white rounded-sm cursor-pointer relative overflow-hidden`};
+
+  ::after {
+    content: "";
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    top: 0;
+    left: 0;
+    clip-path: circle(0% at 0% 0%);
+    background: ${(p) => p.iconColor};
+    transition: clip-path 300ms ease;
+    ${tw`z-10`}
+  }
+  svg {
+    font-size: 2rem;
+    fill: ${(p) => p.iconColor};
+    ${tw`z-20 relative`}
+    transition: fill 250ms ease 50ms;
+  }
+
+  :hover,
+  :focus {
+    svg {
+      fill: #fff;
+    }
+
+    ::after {
+      clip-path: circle(150% at 0% 0%);
+    }
+  }
+`;
+
+export { MediaIcon };
--- src/components/layouts/Footer.tsx
+++ src/components/layouts/Footer.tsx
@@ -1,6 +1,9 @@
 import React, { ReactElement } from "react";
 import styled from "styled-components";
 import tw from "twin.macro";
+import { FaGithubAlt, FaFacebookF, FaLinkedinIn } from "react-icons/fa";
+import { IconType } from "react-icons";
+import { MediaIcon, MediaIconProps } from "../footer/MediaIcon";
 
 type Props = ContainerProps & {};
 
@@ -8,7 +11,40 @@ type Props = ContainerProps & {};
  * @description renders page's footer
  */
 function Footer({ height }: Props): ReactElement {
-  return <Container height={height}></Container>;
+  return (
+    <Container height={height}>
+      <MediaIcons>{renderMediaIcons()}</MediaIcons>
+    </Container>
+  );
+}
+
+function renderMediaIcons() {
+  const mediaIcons: MediaIconProps[] = [
+    {
+      Icon: FaGithubAlt,
+      iconColor: "#6e5494",
+      href: "https://github.com/Andrewnt219",
+      label: "Link to Github profile",
+    },
+    {
+      Icon: FaFacebookF,
+      iconColor: "#3b5998",
+      href: "https://www.facebook.com/phong.nguyentuan.108",
+      label: "Link to Facebook profile",
+    },
+    {
+      Icon: FaLinkedinIn,
+      iconColor: "#0e76a8",
+      href: "https://www.linkedin.com/in/andrewnt219/",
+      label: "Link to LinkedIn profile",
+    },
+  ];
+
+  return mediaIcons.map((mediaIconProps, index) => (
+    <li key={index}>
+      <MediaIcon {...mediaIconProps} />
+    </li>
+  ));
 }
 
 type ContainerProps = {
@@ -16,7 +52,12 @@ type ContainerProps = {
 };
 const Container = styled.footer<ContainerProps>`
   height: ${(p) => p.height};
-  ${tw`bg-lprimary sticky bottom-0 left-0 w-full transition-all duration-300 ease-in-out`};
+  ${tw`bg-lprimary sticky bottom-0 left-0 w-full transition-all duration-300 ease-in-out `};
+`;
+
+type MediaIconsProps = {};
+const MediaIcons = styled.ul<MediaIconsProps>`
+  ${tw`flex  space-x-3`}
 `;
 
 export { Footer };

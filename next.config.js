38ca8232db5735ef09ab934365b35d7de71c4ca4
Tuan Phong Nguyen <phongnguyentuan20@gmail.com>
2020-08-25 23:32:11 -0400

    excluded imgs, og-image from precaching

--- next.config.js
+++ next.config.js
@@ -28,6 +28,7 @@ module.exports = withPlugins(
           dest: "public",
           disable: process.env.NODE_ENV === "development",
           buildExcludes: [/\/images\/.*$/],
+          publicExcludes: ["!imgs", "!og-image.jpg"],
         },
       },
     ],

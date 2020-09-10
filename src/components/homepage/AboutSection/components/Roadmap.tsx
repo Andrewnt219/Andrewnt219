ba652b218a1ca4ad5019031fc24b8bbf80fb52aa
Tuan Phong Nguyen <phongnguyentuan20@gmail.com>
2020-09-10 13:52:50 -0400

    fixed buggy display in Roadmap paragraphs

--- src/components/homepage/AboutSection/components/Roadmap.tsx
+++ src/components/homepage/AboutSection/components/Roadmap.tsx
@@ -11,58 +11,71 @@ function Roadmap(): ReactElement {
     <Container>
       <LazyLoad>
         <ThumbnailContainer>
-          <Thumbnail path={src} alt={alt} sizes="60vw" />
+          {/* NOTE Intentionally set smaller sizes, because the image is blurred anyway */}
+          <Thumbnail path={src} alt={alt} sizes="20vw" />
           <ThumbnailTitle>Road to web&nbsp;development</ThumbnailTitle>
         </ThumbnailContainer>
       </LazyLoad>
 
-      <Paragraph>
-        <ParagraphTitle>The year 2020</ParagraphTitle>I will tell you an
-        incredible story, the story of how I &quot;met&quot; web development.{" "}
-        <EffectText>camera flashback</EffectText>
-      </Paragraph>
+      <ParagraphContainer>
+        <ParagraphTitle>The year 2020</ParagraphTitle>
+        <p>
+          I will tell you an incredible story, the story of how I
+          &quot;met&quot; web development.{" "}
+          <EffectText>camera&nbsp;flashback</EffectText>
+        </p>
+      </ParagraphContainer>
 
-      <Paragraph>
+      <ParagraphContainer>
         <ParagraphTitle>Year 2015</ParagraphTitle>
-        When I first learned Pascal back in highschool, I knew...{" "}
-        <EmphasizedText>I was bad at programming.</EmphasizedText> I had a hard
-        time expressing what I wanted to the machine.
-      </Paragraph>
+        <p>
+          When I first learned Pascal back in highschool, I knew...{" "}
+          <EmphasizedText>I was bad at programming.</EmphasizedText> I had a
+          hard time expressing what I wanted to the&nbsp;machine.
+        </p>
+      </ParagraphContainer>
 
-      <Paragraph>
-        <ParagraphTitle>Year 2017</ParagraphTitle> I came across the webdev
-        subreddit, which was{" "}
-        <EmphasizedText>
-          full of amazing things people could do on the web.
-        </EmphasizedText>{" "}
-        Out of curiosity, I learned HTML and CSS, but quickly moved on after
-        boxes refused to be centered.
-      </Paragraph>
+      <ParagraphContainer>
+        <ParagraphTitle>Year 2017</ParagraphTitle>{" "}
+        <p>
+          {" "}
+          I came across the webdev subreddit, which was{" "}
+          <EmphasizedText>
+            full of amazing things people could do on the web.
+          </EmphasizedText>{" "}
+          Out of curiosity, I learned HTML and CSS, but quickly moved on after
+          boxes refused to be&nbsp;centered.
+        </p>
+      </ParagraphContainer>
 
-      <Paragraph>
-        <ParagraphTitle>Year 2018</ParagraphTitle> Eventually, I still picked IT
-        as my major since computers were what I was most familiar with. To get
-        myself prepared for college, I attended a beginner class for
-        programming. They taught me C#, and I could say{" "}
-        <EmphasizedText>
-          I did not do any better than I did with Pascal.
-        </EmphasizedText>
-      </Paragraph>
+      <ParagraphContainer>
+        <ParagraphTitle>Year 2018</ParagraphTitle>{" "}
+        <p>
+          Eventually, I still picked IT as my major since computers were what I
+          was most familiar with. To get myself prepared for college, I attended
+          a beginner class for programming. They taught me C#, and I could say{" "}
+          <EmphasizedText>
+            I did not do any better than I did with&nbsp;Pascal.
+          </EmphasizedText>
+        </p>
+      </ParagraphContainer>
 
-      <Paragraph>
-        <ParagraphTitle>Year 2019</ParagraphTitle> Believing that anything can
-        be achieved with enough willpower, I passed all courses with an A or
-        higher.{" "}
-        <EmphasizedText>
-          But something was defnitely missing. It was... passion.
-        </EmphasizedText>{" "}
-        I had planned to switch to Hospitality because of how much I liked to
-        interact with people, but <em>the universe</em> told me not to. In my
-        second semester, a friend invited me to join her in one lecture. Little
-        did I know, my whole life just turned to a new chapter.
-      </Paragraph>
+      <ParagraphContainer>
+        <ParagraphTitle>Year 2019</ParagraphTitle>
+        <p>
+          Believing that anything can be achieved with enough willpower, I
+          passed all courses with an A or higher.{" "}
+          <EmphasizedText>
+            But something was defnitely missing. It was... passion.
+          </EmphasizedText>{" "}
+          I had planned to switch to Hospitality because of how much I liked to
+          interact with people, but <em>the universe</em> told me not to. In my
+          second semester, a friend invited me to join her in one lecture.
+          Little did I know, my whole life just turned to a new&nbsp;chapter.
+        </p>
+      </ParagraphContainer>
 
-      <Paragraph>
+      <ParagraphContainer>
         <ParagraphTitle>Now</ParagraphTitle> To this day, I am still deeply
         grateful to {/* TODO added David Humphrey achievement */}
         <a
@@ -81,8 +94,8 @@ function Roadmap(): ReactElement {
         a better one. Sometimes, it feels frightening with so many things are
         going on in the world of web, but seeing my favorite stacks get updated,
         or all the shiny new toys I can play with, are just some of the good
-        vibes to wake up to.
-      </Paragraph>
+        vibes to wake up&nbsp;to.
+      </ParagraphContainer>
     </Container>
   );
 }
@@ -134,11 +147,11 @@ const Thumbnail = styled(ResponsiveImage)`
   }
 `;
 
-type ParagraphProps = {};
-const Paragraph = styled.p<ParagraphProps>``;
+type ParagraphConainer = {};
+const ParagraphContainer = styled.div<ParagraphConainer>``;
 
 type ParagraphTitleProps = {};
-const ParagraphTitle = styled.p<ParagraphTitleProps>`
+const ParagraphTitle = styled.h4<ParagraphTitleProps>`
   ${tw`text-ltextColor block relative flex items-center`}
 
   ::before {

bdb238309299493dbf1a704a7df59a57e56ba7f2
Tuan Phong Nguyen <phongnguyentuan20@gmail.com>
2020-09-11 18:38:56 -0400

    moved images outside of public

--- next.config.js
+++ next.config.js
@@ -42,7 +42,7 @@ module.exports = withPlugins(
       config.resolve.modules.push(__dirname);
 
       // for next-optimized-images
-      config.resolve.alias.images = path.join(__dirname, "/public/images");
+      config.resolve.alias.images = path.join(__dirname, "/images");
 
       // for dev liniting in terminal
       if (dev) {

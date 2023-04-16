2a5e53bb96c0fe9fa3ad4fced173b02f56f08d1e
Andrew Nguyen <52666982+Andrewnt219@users.noreply.github.com>
2023-04-16 14:15:01 -0400

    delete v1 source coded

--- src/constants/ga.constants.ts
+++
@@ -1,8 +0,0 @@
-export enum GaCategories {
-  Hero = "Hero Section",
-  About = "About",
-  Contact = "Contact",
-  Projects = "Projects",
-  Achievements = "Achievements",
-  Sidebar = "Sidebar",
-}
--- src/constants/personalInfo.constants.ts
+++
@@ -1,7 +0,0 @@
-export enum PersonalInfo {
-  Email = "hey@andrewnt.dev",
-  Phone = "(647) 745-4643",
-  Facebook = "https://www.facebook.com/phong.nguyentuan.108",
-  GitHub = "https://github.com/Andrewnt219",
-  LinkedIn = "https://www.linkedin.com/in/andrewnt219/",
-}
